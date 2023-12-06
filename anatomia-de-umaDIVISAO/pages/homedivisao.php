6ad72a2cc2c05347d68a49e72f31792b3561282a
Robson Barros <robsonfullstackweb@gmail.com>
2023-12-06 13:47:41 -0300

    adicionei pagina para divisão,modifiquei o header linkando os estilos,e apliquei estilos para a tabela divisão

--- anatomia-de-umaDIVISAO/pages/homedivisao.php
+++ anatomia-de-umaDIVISAO/pages/homedivisao.php
@@ -12,15 +12,31 @@ $divisor = $_GET['d2'] ?? 1;
 
 <form method="GET">
 	<label for="d1" name="d1" id="d1">Dividendo</label>
-	<input type="number" name="d1" id="d1" value="<?=$dividendo?>">
+	<input type="number" name="d1" id="d1" min="0" value="<?=$dividendo?>">
 	<label>Divisor</label>
-	<input type="number" name="d2" id="d2" value="<?=$divisor?>">
+	<input type="number" name="d2" id="d2" min="1"value="<?=$divisor?>">
 	<input type="submit" name="analisar" value="Analizar">
 </form>
 <section>
 	<h2>ESTRUTURA DA DIVISÃO</h2>
+<?php
+
+$quociente = intdiv($dividendo, $divisor);
+$resto = $dividendo % $divisor;
 
 
+
+?>
+<table class="divisao">
+	<tr>
+		<td><?=$dividendo?></td>
+		<td><?=$divisor?></td>
+	</tr>
+	<tr>
+		<td><?=$resto?></td>
+		<td><?=$quociente?></td>
+	</tr>
+</table>
 
 </section>
 </main>
