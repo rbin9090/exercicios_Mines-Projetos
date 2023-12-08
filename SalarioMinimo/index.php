725e5c0133aa61e2f751b4613d1b4e7e413c9e50
Robson Barros <robsonfullstackweb@gmail.com>
2023-12-07 23:58:31 -0300

    utilizando formulario retroalimentado sistema rodando para calcular salarios

--- SalarioMinimo/index.php
+++ SalarioMinimo/index.php
@@ -14,6 +14,7 @@
 <?php
 
 $minimo = 1_320.00;
+$salario = $_GET['sal'] ?? 0;
 
 
 
@@ -23,13 +24,25 @@ $minimo = 1_320.00;
 	
 <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
 	<label for="sal">Salário (R$)</label>
-	<input type="number" name="sal" id="sal" value="" step="0.01">
+	<input type="number" name="sal" id="sal" value="<?=$salario?>" step="0.01">
 	<input type="submit" value="calcular">
 <p>Considerando o salario minimo de R$ <?=number_format($minimo,2,",",".")?></p>
 
 </form>
 
+<section>
+	
+	<h2>Resultado final: </h2>
+	<?php
 
+	$total = intdiv($salario,$minimo);
+	$dif = $salario % $minimo;
+
+echo ("ganha <strong>$total</strong> salarios Minimos");
+
+	?>
+
+</section>
 
 
 </main>
